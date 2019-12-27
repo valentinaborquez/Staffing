6383ea36e5c6bfec0b583bec92c63bf0983ee27e
Millena13 <millena.yissell@gmail.com>
2019-12-27 13:46:15 -0300

    crearp3

---
+++ ProyectoStaffing/Pioners/src/test/java/com/pioners/Pioners/PionersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioners.Pioners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PionersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
